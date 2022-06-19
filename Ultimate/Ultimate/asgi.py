afcfbea91fb0de2badf115cf2fa73fb82ae438e7
Damilare Olayiwola <olayiwolaayomide760@gmail.com>
2022-06-19 08:51:27 +0100

    creating a django app

---
+++ Ultimate/Ultimate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ultimate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ultimate.settings')
+
+application = get_asgi_application()
